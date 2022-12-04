c033c10b3861851f438298a3d19169fbd80d0dc7
StormdayGG <32097345+StormdayGG@users.noreply.github.com>
2022-12-04 20:18:20 +0200

    Updated Connections To Database

--- createPost.php
+++ createPost.php
@@ -8,14 +8,15 @@
 	}
 	function submit_form($title, $text, $ingredients, $quantities, $tags)
 	{
-		$servername = "localhost";
-		$username = "root";
-		$password = "alpiAlbut";
-		$dbname = "sweet_corner";
-		
-		
-			
-		$conn = new mysqli($servername, $username, $password, $dbname);
+		$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+		$cleardb_server = $cleardb_url["host"];
+		$cleardb_username = $cleardb_url["user"];
+		$cleardb_password = $cleardb_url["pass"];
+		$cleardb_db = substr($cleardb_url["path"],1);
+		$active_group = 'default';
+		$query_builder = TRUE;
+		// Connect to DB
+		$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 		if($conn -> connect_error) {
 			die("Connection failed: ".$conn->connect_error);
 		}
--- viewRecipe.php
+++ viewRecipe.php
@@ -9,13 +9,16 @@
 	if(!empty($_GET["id"]))
 	{
 		$recipe_id = $_GET["id"];
-		$servername = "localhost";
-		$username = "root";
-		$password = "alpiAlbut";
-		$dbname = "sweet_corner";
-		
-		
-		$conn = new mysqli($servername, $username, $password, $dbname);
+		$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+		$cleardb_server = $cleardb_url["host"];
+		$cleardb_username = $cleardb_url["user"];
+		$cleardb_password = $cleardb_url["pass"];
+		$cleardb_db = substr($cleardb_url["path"],1);
+		$active_group = 'default';
+		$query_builder = TRUE;
+		// Connect to DB
+		$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 		if($conn -> connect_error) {
 			die("Connection failed: ".$conn->connect_error);
 		}
